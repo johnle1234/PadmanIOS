1388424fa2a46d147a3840946ca2bfcc88c718dd
johnle1234 <johnathanl1234@gmail.com>
2014-09-01 17:40:48 -0700

    Initial local commit

---
+++ PadmanIOSTests/PadmanIOSTests.m
@@ -0,0 +1,34 @@
+//
+//  PadmanIOSTests.m
+//  PadmanIOSTests
+//
+//  Created by Johnathan Le on 9/1/14.
+//  Copyright (c) 2014 johnle. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PadmanIOSTests : XCTestCase
+
+@end
+
+@implementation PadmanIOSTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
